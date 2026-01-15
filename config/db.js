1534649e91c2bd326bcb57abff5bf8ce7c91a376
Akash Patankar <akashpatankar987@gmail.com>
2026-01-15 14:27:56 +0530

    fixes

--- config/db.js
+++ config/db.js
@@ -1,21 +1,11 @@
 import mysql from "mysql2/promise";
-import { URL } from "url";
-
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not set");
-}
-
-const url = new URL(process.env.DATABASE_URL);
 
 export const pool = mysql.createPool({
-  host: url.hostname,
-  user: url.username,
-  password: url.password,
-  database: url.pathname.slice(1), // 🔥 "railway"
-  port: Number(url.port),
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
   connectTimeout: 30000,
 });
 
